e10499289dfafac0de23a7d1f39ed4c7e48f74cb
Mo McRoberts <mo.mcroberts@nexgenta.com>
2010-01-13 18:03:04 +0000

    Only allow index.setup to be invoked from the CLI.

--- index.setup.php
+++ index.setup.php
@@ -1,5 +1,10 @@
 <?php
 
+if(php_sapi_name() != 'cli')
+{
+	die('Error: index.setup.php may only be invoked from the command-line');
+}
+
 define('INSTANCE_ROOT', realpath(getcwd()) . '/');
 
 require(dirname(__FILE__) . '/platform.php');
