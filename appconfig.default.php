00dfed844366999dcae2cfcd2d3b87de866bf585
Mo McRoberts <mo.mcroberts@nexgenta.com>
2010-03-02 22:44:19 +0000

    Add a default appconfig to go alongside config.default.php (for manual installation instructions).

---
+++ appconfig.default.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Application configuration */
+
+/* This is a placeholder file - copy it to the <root>/config directory 
+ * and symbolically link appconfig.php to it.
+ */
+
+$HTTP_ROUTES = array(
+	'__NONE__' => array('class' => 'Page', 'templateName' => 'home.phtml', 'title' => 'Sample homepage'),
+	'test' => array('class' => 'Page', 'templateName' => 'test.phtml', 'title' => 'Another sample page'),
+);
