56c5cb8f5457d808428c56408f797ea1a805ecd1
nevali <mo@nevali.net>
2011-03-06 20:36:41 +0000

    Set PUBLIC_ROOT to INSTANCE_ROOT/public if applicable

--- index.install.php
+++ index.install.php
@@ -2,10 +2,19 @@
 
 if(php_sapi_name() != 'cli')
 {
-	die('Error: index.setup.php may only be invoked from the command-line');
+	die('Error: index.install.php may only be invoked from the command-line');
 }
 
 define('INSTANCE_ROOT', realpath(getcwd()) . '/');
+
+if(file_exists(INSTANCE_ROOT . 'public') || !file_exists(INSTANCE_ROOT . 'index.pho'))
+{
+	define('PUBLIC_ROOT', INSTANCE_ROOT . 'public/');
+}
+else
+{
+	define('PUBLIC_ROOT', INSTANCE_ROOT);
+}
 
 if(count($argv) == 1)
 {
