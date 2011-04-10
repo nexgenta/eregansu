e5fb7a9d4b1c28f95e66519b62cc51dcddbcb85d
Mo McRoberts <mo@nevali.net>
2011-04-11 00:11:49 +0100

    Bump copyright date

--- lib/common.php
+++ lib/common.php
@@ -1,6 +1,6 @@
 <?php
 
-/* Copyright 2009, 2010 Mo McRoberts.
+/* Copyright 2009-2011 Mo McRoberts.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
