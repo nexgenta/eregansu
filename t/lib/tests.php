1d54c692a462d4d044d01c02768848a33d24034f
Mo McRoberts <mo@nevali.net>
2011-07-03 10:44:04 +0100

    Use new test harness (#41)

--- t/lib/tests.php
+++ t/lib/tests.php
@@ -15,139 +15,17 @@
  *  limitations under the License.
  */
 
-require_once(dirname(__FILE__) . '/../../lib/common.php');
-
+define('EREGANSU_SKIP_CONFIG', true);
 define('LOGFILE', 'tests.log');
 
-$php_path = 'php';
-$s = getenv('PHP_5');
-if(strlen($s))
-{
-	$php_path = $s;
-}   
-$tests = array();	
-$counts = array('total' => 0, 'pass' => 0, 'fail' => 0, 'xpass' => 0, 'xfail' => 0);
-$root = simplexml_load_file('tests.xml');
-if(!is_object($root)) exit(1);
+require_once(dirname(__FILE__) . '/../../platform.php');
+require_once(PLATFORM_ROOT . 'testsuite/testsuite.php');
 
-$f = fopen(LOGFILE, 'w');
-fwrite($f, "Test run beginning at " . strftime('%Y-%m-%dT%H:%M:%SZ') . "\n");
-fwrite($f, str_repeat('=', 72) . "\n");
-fclose($f);
-
-foreach($root->test as $t)
+$suite = TestSuite::suiteFromXML('tests.xml', LOGFILE);
+$suite->issuesUrl = 'http://github.com/nexgenta/eregansu/issues/';
+$suite->run();
+if($suite->uxpass || $suite->uxfail)
 {
-	$attrs = $t->attributes();
-	$expect = 'pass';
-	$name = strval($t);
-	$issue = null;
-	if(isset($attrs->expect))
-	{
-		$expect = strval($attrs->expect);
-	}
-	if(isset($attrs->issue))
-	{
-		$issue = strval($attrs->issue);
-	}
-	$tests[] = array('name' => $name, 'expect' => strtolower($expect), 'issue' => $issue);
+	exit(1);
 }
-
-foreach($tests as $test)
-{
-	runTest($test);
-}
-
-$uxpass = $counts['pass'] - $counts['xpass'];
-$uxfail = $counts['fail'] - $counts['xfail'];
-
-$buf = "Total: " . $counts['total'] . ' -- ';
-$buf .= $counts['pass'] . ' passed' . ($uxpass ? ' (' . $uxpass . ' unexpected)' : '') . ', ';
-$buf .= $counts['fail'] . ' failed' . ($uxfail ? ' (' . $uxfail . ' unexpected)' : '') . ".\n";
-
-$f = fopen(LOGFILE, 'a');
-fwrite($f, "Test run completed at " . strftime('%Y-%m-%dT%H:%M:%SZ') . "\n");
-fwrite($f, $buf);
-fclose($f);
-
-echo $buf;
-
-if(!$uxpass && !$uxfail)
-{
-	exit(0);
-}
-exit(1);
-
-function runTest($test)
-{
-	global $counts, $php_path;
-
-	$f = fopen(LOGFILE, 'a');
-	fprintf($f, "%s - Executing test: %s, expected result: %s\n", strftime('%Y-%m-%dT%H:%M:%SZ'), $test['name'], $test['expect']);
-	if(strlen($test['issue']))
-	{
-		fprintf($f, "Issue URL: http://github.com/nexgenta/eregansu/issues/%s\n", $test['issue']);
-	}
-	$counts['total']++;
-	echo sprintf('%20s', $test['name']) . " ... ";
-	$status = 256;
-	if(file_exists($test['name']))
-	{		
-		fwrite($f, str_repeat('-', 72) . "\n"); 
-		fclose($f);
-		$result = system($php_path . ' -f ' . escapeshellarg($test['name']) . ' >>tests.log 2>&1', $status);
-		if($result === false)
-		{
-			$status = 256;
-		}
-		$f = fopen(LOGFILE, 'a');
-		if($status)
-		{
-			fwrite($f, str_repeat('-', 72) . "\n");
-		}
-		fprintf($f, "%s - Test %s completed with status %d\n", strftime('%Y-%m-%dT%H:%M:%SZ'), $test['name'], $status);
-	}
-	else
-	{
-		fwrite($f, "Test cannot be executed because " . $test['name'] . " does not exist\n");
-	}
-	fwrite($f, str_repeat('=', 72) . "\n");
-	fclose($f);
-	if($status > 254)
-	{
-		echo "*** ERROR *** (failed to execute test)";
-		$counts['fail']++;
-	}
-	else if($status == 0)
-	{
-		$counts['pass']++;
-		if($test['expect'] == 'pass')
-		{
-			echo "PASS";
-			$counts['xpass']++;
-		}
-		else
-		{
-			echo "UXPASS";
-		}
-	}
-	else
-	{
-		$counts['fail']++;
-		if($test['expect'] == 'fail')
-		{
-			echo "XFAIL";
-			$counts['xfail']++;
-			$status = 0;
-			if(strlen($test['issue']))
-			{
-				echo " -- http://github.com/nexgenta/eregansu/issues/" . $test['issue'];
-			}
-		}
-		else
-		{
-			echo "FAIL ($status)\n";
-		}
-	}
-	echo "\n";
-	return $status;
-}
+exit(0);
