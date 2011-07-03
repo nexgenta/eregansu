f7034bae5cfae3f0435882edbd6e93bbfa8b89e7
Mo McRoberts <mo@nevali.net>
2011-07-03 11:13:40 +0100

    Move well-known namespace constants up from RDF to XMLNS (closes #43).

--- lib/rdf.php
+++ lib/rdf.php
@@ -21,23 +21,6 @@ require_once(dirname(__FILE__) . '/url.php');
 
 abstract class RDF extends XMLNS
 {
-	const rdf = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#';
-	const rdfs = 'http://www.w3.org/2000/01/rdf-schema#';
-	const owl = 'http://www.w3.org/2002/07/owl#';
-	const foaf = 'http://xmlns.com/foaf/0.1/';
-	const skos = 'http://www.w3.org/2008/05/skos#';
-	const time = 'http://www.w3.org/2006/time#';
-	const rdfg = 'http://www.w3.org/2004/03/trix/rdfg-1/';
-	const event = 'http://purl.org/NET/c4dm/event.owl#';
-	const frbr = 'http://purl.org/vocab/frbr/core#';
-	const dcmit = 'http://purl.org/dc/dcmitype/';
-	const geo = 'http://www.w3.org/2003/01/geo/wgs84_pos#';
-	const mo = 'http://purl.org/ontology/mo/';
-	const theatre = 'http://purl.org/theatre#';
-	const participation = 'http://purl.org/vocab/participation/schema#';
-	const xhv = 'http://www.w3.org/1999/xhtml/vocab#';
-	const gn = 'http://www.geonames.org/ontology#';
-
 	/* Registered ontology handlers */
 	public static $ontologies = array();
 	/* Registered namespaces */
