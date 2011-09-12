405747b5f892de1b73579185becbf389553075f2
Mo McRoberts <mo@nevali.net>
2011-09-12 09:08:29 +0100

    Support (indeed, prefer) module schemata to be named 'schema.php' in preference to 'module.php'.

--- platform/cli.php
+++ platform/cli.php
@@ -104,7 +104,14 @@ class CliSetup extends CommandLine
 		{
 			if(!isset($mod['file']))
 			{
-				$mod['file'] = 'module.php';
+				if(file_exists(MODULES_ROOT . $mod['name'] . '/module.php'))
+				{
+					$mod['file'] = 'module.php';
+				}
+				else
+				{
+					$mod['file'] = 'schema.php';
+				}
 			}
 			if(!isset($mod['class']))
 			{
